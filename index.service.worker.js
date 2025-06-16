f7009dbf73732944d10a76ad00a333e84e4cf2bd
titenq <titenq@gmail.com>
2025-06-16 19:08:53 -0300

    pong

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1750110708|5770584479';
+const CACHE_VERSION = '1750111711|6773694112';
 /** @type {string} */
 const CACHE_PREFIX = 'pong-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
